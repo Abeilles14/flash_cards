5c56f1714ca33d1b8e0cd4d7ffc5e5011735af06
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2019-12-23 02:52:45 -0500

    creating drawer side menu

---
+++ app/src/main/java/com/isabelle/flash/MainActivity.java
@@ -0,0 +1,28 @@
+package com.isabelle.flash;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             //listens to activity main
+
+        Toolbar toolbar = findViewById(R.id.toolbar);   //toolbar drawer menu
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);      //button opens drawer menu
+
+
+    }
+}
