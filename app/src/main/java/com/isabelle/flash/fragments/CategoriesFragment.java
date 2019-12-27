aa1a9fc80c723f31cf73c1aca32cb4136b86a870
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2019-12-27 16:28:07 -0500

    app starts now, crashes at categories

---
+++ app/src/main/java/com/isabelle/flash/fragments/CategoriesFragment.java
@@ -0,0 +1,70 @@
+package com.isabelle.flash.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.isabelle.flash.R;
+//import com.isabelle.flash.adapters.CategoryAdapter;
+import com.isabelle.flash.adapters.CategoryAdapter;
+import com.isabelle.flash.fab.FloatingActionButton;
+import com.isabelle.flash.models.Category;
+import java.util.List;
+
+public class CategoriesFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter category_adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private View view;
+    private List<Category> categories;
+    private FloatingActionButton buttonFab;
+
+    public CategoriesFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        view = inflater.inflate(R.layout.fragment_categories,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //initialize FloatingActionButton and set colors
+        buttonFab = (FloatingActionButton) view.findViewById(R.id.addNewCategory);
+        buttonFab.setColor((getResources().getColor(R.color.action_bar_color)));
+        buttonFab.setTextColor(getResources().getColor(R.color.action_bar_text_color));
+
+        //initialize recycler view from fragment_category
+        recyclerView = view.findViewById(R.id.list_cards);
+        recyclerView.setHasFixedSize(true);
+
+        //initialize layout manager
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        //TO DELETE
+        //new instances of example lists
+//        categories = new ArrayList<Category>();
+//        categories.add(new Category("Kanji", "Memorize new Japanese Kanji Characters"));
+//        categories.add(new Category("Vocabulary", "Rehearse Japanese words"));
+
+        //creating instances of categories, adding to array list
+        for (int i = 0; i < 10; i++) {
+            Category category = new Category("Category " + i,"Category "+ i + " Description");
+            categories.add(category);
+        }
+
+        category_adapter = new CategoryAdapter(this.getActivity(),categories);
+        recyclerView.setAdapter(category_adapter);
+    }
+}
--- app/src/main/java/com/isabelle/flash/fragments/ProfileFragment.java
+++ app/src/main/java/com/isabelle/flash/fragments/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.isabelle.flash;
+package com.isabelle.flash.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,11 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class CategoriesFragment extends Fragment {
+import com.isabelle.flash.R;
+
+public class ProfileFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_categories,container,false);
+        return inflater.inflate(R.layout.fragment_profile,container,false);
     }
 }
