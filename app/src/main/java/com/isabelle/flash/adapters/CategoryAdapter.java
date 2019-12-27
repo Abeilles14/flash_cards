47441ad60e8dfc364009755d20f339d309f226b9
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2019-12-27 00:08:26 -0500

    used recyclerview to create category list

---
+++ app/src/main/java/com/isabelle/flash/adapters/CategoryAdapter.java
@@ -0,0 +1,76 @@
+package com.isabelle.flash.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.isabelle.flash.R;
+import com.isabelle.flash.models.Category;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private ArrayList<Category> categories;
+
+    //constructor
+    //context = activity
+    public CategoryAdapter(Context context, ArrayList<Category> list){
+        categories = list;
+    }
+
+    //single row/card in fragment_category
+    //if item in list gets clicked, will activate this class
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView category_name, category_description;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //retrieve views from xml
+            //extracts individual itemView from ViewHolder, text
+            category_name = itemView.findViewById(R.id.category_name);
+            category_description = itemView.findViewById(R.id.category_description);
+
+            //on click
+            itemView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+    }
+
+    //gets layout of the category/row
+    @NonNull
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //referring to fragment_categories
+        //connected to horizontal layout/row
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_categories,viewGroup,false);
+        return new ViewHolder(v);   //returns to ViewHolder with the layout of the category row
+    }
+
+    //set text on view
+    //run for amount of viewHolders/categories in array
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder viewHolder, int i) {
+        //get specific list item tag
+        viewHolder.itemView.setTag(categories.get(i));  //use tag in onClick to check which clicked
+
+        viewHolder.category_name.setText(categories.get(i).getName());
+        viewHolder.category_description.setText(categories.get(i).getDescription());
+    }
+
+    //get number of categories in array list
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+}
--- app/src/main/java/com/isabelle/flash/fragments/CategoriesFragment.java
+++ app/src/main/java/com/isabelle/flash/fragments/CategoriesFragment.java
@@ -1,20 +1,63 @@
 package com.isabelle.flash.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.isabelle.flash.R;
+import com.isabelle.flash.adapters.CategoryAdapter;
+import com.isabelle.flash.models.Category;
+
+import java.util.ArrayList;
 
 public class CategoriesFragment extends Fragment {
 
-    @Nullable
+    RecyclerView recyclerView;
+    RecyclerView.Adapter category_adapter;
+    RecyclerView.LayoutManager layoutManager;
+    View view;
+    private ArrayList<Category> categories;
+
+    public CategoriesFragment() {
+
+    }
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_categories,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_categories,container,false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.list_cards);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        //TO DELETE
+        //new instances of example lists
+        categories = new ArrayList<Category>();
+        categories.add(new Category("Kanji", "Memorize new Japanese Kanji Characters"));
+        categories.add(new Category("Vocabulary", "Rehearse Japanese words"));
+
+//        for (int i = 0; i < 10; i++) {
+//            Category category = new Category(getActivity());
+//            category.setName(i);
+//            category.setDescription(i);
+//            categories.add(new Category);
+//        }
+
+        category_adapter = new CategoryAdapter(this.getActivity(),categories);
+        recyclerView.setAdapter(category_adapter);
     }
 }
